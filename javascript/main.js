f37739aca3f0433c7a274db2f7ad6ffc9151caa0
mono-tmt <tomita@monosus.co.jp>
2021-04-19 20:26:48 +0900

    :+1: 練習問題1
    t

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+// 練習問題
+
+// using if
+if (c == 1) {
+  console.log("case 1")
+} else if (c==2) {
+  console.log("case 2")
+} else {
+  console.log("default")
+  console.log("case 2")
+}
+
+// using switch
+switch (c) {
+  case (c == 1):
+    console.log("case 1")
+  case (c == 2):
+    console.log("case 2")
+  default:
+    console.log("default")
+    console.log("case 2")
+}
