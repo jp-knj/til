440454d3c6f40e1b565027c48fc77f2bae8f2289
jp-knj <kenji.tomita1996@gmail.com>
2021-04-22 08:21:58 +0900

    make fizzbuzz fn
    t

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,13 @@ function makeFizzBuzzIterator(max) {
       count++
       const done = max <= count
       if (count % 6 === 0) {
-        return {value: "FizzBuzz", done}
+        return { value: "FizzBuzz", done }
+      } else if (count % 2 === 0) {
+        return { value: "Fizz", done }
+      } else if (count % 3 === 0) {
+        return { value: "Buzz", done }
+      } else {
+        return { value: count, done }
       }
     }
   }
