12027e50ff28a723ecb5baa98f7bd405ac2d8ff1
jp-knj <kenji.tomita1996@gmail.com>
2021-05-26 06:52:15 +0900

    :tada: Learn about HTTP and HTTPS

--- node/server.js
+++ node/server.js
@@ -9,3 +9,27 @@ require("http").createServer((inRequest, inResponse) => {
     }
   );
 }).listen(80);
+
+// Example
+let finalResponse = "";
+const request = require("http").request(
+  {
+    hostname: "www.some_remote_system.com",
+    port: 80,
+    path: "/someAPI",
+    method: "POST"
+  },
+  (inResponse) => {
+    console.log(`STATUS: ${inResponse.statusCode}`);
+    inResponse.setEncoding("utf8");
+    inResponse.on("data", (inDataChunk) => {
+      finalResponse += inDataChunk
+    });
+    inResponse.on("end", () => {
+      console.log(finalResponse);
+    });
+  }
+);
+
+request.write("Some data to send to the remote system");
+request.end();
